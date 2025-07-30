067af4037985eb4f9d1b67f82e431c0cbe155e21
CaioS1lv2 <164821724+CaioS1lv2@users.noreply.github.com>
2025-07-30 19:45:25 -0300

    novas aulas

---
+++ Aula7/Aula7.py
@@ -0,0 +1,3 @@
+n = int(input("Digite um numero: "))
+z = int(input("Digite um numero: "))
+print(f'A soma de {n} e {z} é igual a {n + z}')
---
+++ Aula8/aula.py
@@ -0,0 +1,16 @@
+import emoji
+print(emoji.emojize('Ola, mundo'))
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print(f'A rwaiz de {num} é igual a {raiz:.2f}')
+
+# # import sqrt from math
+# from math import sqrt, floor
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print(f'A raiz de {num} é igual a {raiz:.2f}')
+
+#import random
+#num = random.random()
+#print(f'O número sorteado foi {num}')
