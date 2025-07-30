5a5953b11f17394271d0154951140bc02dfbfde7
CaioS1lv2 <164821724+CaioS1lv2@users.noreply.github.com>
2025-07-30 19:45:42 -0300

    Mais algumas

--- Aula7/Aula7.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Digite um numero: "))
-z = int(input("Digite um numero: "))
-print(f'A soma de {n} e {z} é igual a {n + z}')
--- Aula8/aula.py
+++ Aula8/aula.py
@@ -1,16 +1,15 @@
-import emoji
-print(emoji.emojize('Ola, mundo'))
-# import math
-# num = int(input('Digite um número: '))
-# raiz = math.sqrt(num)
-# print(f'A rwaiz de {num} é igual a {raiz:.2f}')
 
-# # import sqrt from math
-# from math import sqrt, floor
-# num = int(input('Digite um número: '))
-# raiz = sqrt(num)
-# print(f'A raiz de {num} é igual a {raiz:.2f}')
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print(f'A rwaiz de {num} é igual a {raiz:.2f}')
 
-#import random
-#num = random.random()
-#print(f'O número sorteado foi {num}')
+# import sqrt from math
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A raiz de {num} é igual a {raiz:.2f}')
+
+import random
+num = random.random()
+print(f'O número sorteado foi {num}')
